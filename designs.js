1acea459e1723f5593b0bf18b4117e82d329a532
Shannon <13708207+smckinney1@users.noreply.github.com>
2017-10-10 10:52:47 -0400

    Added jQuery to HMTL. Adding table rows now works.

--- designs.js
+++ designs.js
@@ -3,8 +3,35 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
 
-}
+$(function () {
+	function makeGrid(e) {
+
+		//TODO: Allow multiple submissions by clearing out the columns also - just testing rows to start with
+
+		e.preventDefault();
+
+		let rows = Number($('#input_height').val());
+		let columns = $('#input_width').val();
+		let canvas = $('#pixel_canvas');
+
+		$('.art-row').remove();
+
+		//TODO: get color
+
+		for (var i = 0; i < rows; i++) {
+			canvas.append('<tr class="art-row"><td></td></tr>');
+		}
+
+	}
+
+//set separately so color can be changed after form submission 
+	// function getColor() {
+	// 	let color = $('#colorPicker').val();
+	// 	//change color of grid
+	// }
+
+	$('#sizePicker').submit(makeGrid);
+	//$('#colorPicker').change(getColor);
+});
